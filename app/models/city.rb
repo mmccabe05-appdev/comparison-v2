0d87b9214a7799fedfdd41459053d96c6179d71b
mmccabe05 <mmccabe@chicagobooth.edu>
2023-02-04 22:36:31 +0000

    added cities validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,9 @@
 class City < ApplicationRecord
+
+
+  has_many(:neighborhoods, { :class_name => "Neighborhood", :foreign_key => "city_id", :dependent => :destroy })
+
+  has_many(:comparisons_as_neighborhood_1s, { :through => :neighborhoods, :source => :comparisons_as_neighborhood_1 })
+  has_many(:comparisons_as_neighborhood_2s, { :through => :neighborhoods, :source => :comparisons_as_neighborhood_2 })
+
 end
