8f39f77727ddcc6a07ddbb3df09e048641ebbb0f
mmccabe05 <mmccabe@chicagobooth.edu>
2023-02-04 22:37:31 +0000

    added fav neighborhood associations and validations

--- app/models/favorite_neighborhood.rb
+++ app/models/favorite_neighborhood.rb
@@ -1,4 +1,9 @@
 class FavoriteNeighborhood < ApplicationRecord
-  belongs_to :user
-  belongs_to :neighborhood
+  # belongs_to :user
+  # belongs_to :neighborhood
+
+
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id", :counter_cache => true })
+  belongs_to(:neighborhood, { :required => true, :class_name => "Neighborhood", :foreign_key => "neighborhood_id", :counter_cache => true })
+
 end
