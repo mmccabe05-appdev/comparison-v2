ea15ffc57f743d03a8080600a3db2ee84435c0e1
mmccabe05 <mmccabe@chicagobooth.edu>
2023-02-12 20:32:58 +0000

    working with active record relations to return the right comparison neighborhoods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,23 @@ class ApplicationController < ActionController::Base
   end
   
   def find_comparison
-    render({:template=>"found_comparison.html.erb"})
-  end
-  
+
+    if City.find(params.fetch("query_city")) == nil || Neighborhood.find(params.fetch("query_neighborhood")) == nil 
+      redirect_to("/", alert: "Gotta choose a valid neighborhood and city!" )
+
+    else
+
+      @query_city = City.find(params.fetch("query_city"))
+      @query_neighborhood = Neighborhood.find(params.fetch("query_neighborhood"))
+
+      @all_comparisons_for_query_neighborhood = @query_neighborhood.comparisons_as_neighborhood_1.or(@query_neighborhood.comparisons_as_neighborhood_2)
+
+
+      @comparisons_limited_to_target_city = @all_comparisons_for_query_neighborhood.where(:city_1=>@query_city).or(@all_comparisons_for_query_neighborhood.where(:city_2=>@query_city)) 
+      
+      render({:template=>"found_comparison.html.erb"})
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to "/", alert: "Gotta choose a valid neighborhood and city!" 
+  end 
 end
